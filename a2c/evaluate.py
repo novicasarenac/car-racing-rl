980fff3e2b60adcbef1752010b6bda244bfdb22a
novicasarenac <nolesarenac@yahoo.com>
2019-02-02 23:22:58 +0100

    Model evaluation added

---
+++ a2c/evaluate.py
@@ -0,0 +1,33 @@
+import gym
+import torch
+from a2c.actor_critic import ActorCritic
+from a2c.actions import get_action_space, get_actions
+from a2c.environment_wrapper import EnvironmentWrapper
+
+
+def evaluate_a2c(params, path):
+    model = ActorCritic(params.stack_size, get_action_space())
+    model.load_state_dict(torch.load(path))
+    model.eval()
+
+    env = gym.make('CarRacing-v0')
+    env_wrapper = EnvironmentWrapper(env, params.stack_size)
+
+    total_reward = 0
+    num_of_episodes = 100
+
+    for episode in range(num_of_episodes):
+        state = env_wrapper.reset()
+        state = torch.Tensor([state])
+        done = False
+        score = 0
+        while not done:
+            probs, _, _ = model(state)
+            action = get_actions(probs)
+            state, reward, done = env_wrapper.step(action[0])
+            state = torch.Tensor([state])
+            score += reward
+            env_wrapper.render()
+        print('Episode: {0} Score: {1:.2f}'.format(episode, score))
+        total_reward += score
+    return total_reward / num_of_episodes
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import argparse
 from a2c.train import A2CTrainer
 from params import Params
 from a2c.inference import a2c_inference
+from a2c.evaluate import evaluate_a2c
 
 
 def get_trainer(model_type, params):
@@ -22,20 +23,35 @@ def run_inference(model_type):
     if model_type == 'a2c':
         score = a2c_inference(params, 'models/a2c.pt')
 
-    print('Total score: {}'.format(score))
+    print('Total score: {0:.2f}'.format(score))
+
+
+def run_evaluation(model_type):
+    print('evaluating')
+    params = Params('params/' + model_type + '.json')
+    if model_type == 'a2c':
+        score = evaluate_a2c(params, 'models/a2c.pt')
+
+    print('Average reward after 100 episodes: {0:.2f}'.format(score))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--train', required=True,
-                        help='Train model, or run inference.')
     parser.add_argument('-m', '--model', required=True,
                         choices=['a2c', 'a3c', 'dqn'],
                         help='Which model to run / train.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--train', action='store_true',
+                       help='Train model.')
+    group.add_argument('--inference', action='store_true',
+                       help='Model inference.')
+    group.add_argument('--evaluate', action='store_true',
+                       help='Evaluate model on 100 episodes.')
 
     args = vars(parser.parse_args())
-
-    if eval(args['train']):
+    if args['train']:
         run_training(args['model'])
+    elif args['evaluate']:
+        run_evaluation(args['model'])
     else:
         run_inference(args['model'])
