fc9a2b6e63cfb15198e2626f57a81a416f7147da
novicasarenac <nolesarenac@yahoo.com>
2019-04-06 19:28:52 +0200

    Added gradients sharing. Training loop finished.

--- actor_critic/a3c/storage.py
+++ actor_critic/a3c/storage.py
@@ -20,3 +20,11 @@ class Storage:
         self.action_log_probs[step] = action_log_prob
         self.entropies[step] = entropy
         self.dones[step] = done
+
+    def compute_expected_reward(self, last_value, discount_factor):
+        expected_reward = torch.zeros(self.steps_per_update + 1, 1)
+        expected_reward[-1] = last_value
+        for step in reversed(range(self.rewards.size(0))):
+            expected_reward[step] = self.rewards[step] + \
+                                    expected_reward[step + 1] * discount_factor * (1.0 - self.dones[step])
+        return expected_reward[:-1]
--- actor_critic/a3c/worker.py
+++ actor_critic/a3c/worker.py
@@ -1,6 +1,7 @@
 import torch
 import gym
 import torch.multiprocessing as mp
+import torch.nn as nn
 from torch.optim import Adam
 from actor_critic.environment_wrapper import EnvironmentWrapper
 from actor_critic.actor_critic import ActorCritic
@@ -33,11 +34,45 @@ class Worker(mp.Process):
             for step in range(self.params.steps_per_update):
                 probs, log_probs, value = self.model(self.current_observation)
                 action = get_actions(probs)[0]
+                action_log_prob, entropy = self.compute_action_log_and_entropy(probs, log_probs)
 
-                action_log_probs, entropy = self.compute_action_logs_and_entropies(probs, log_probs)
-                break
-            break
+                state, reward, done = self.environment.step(action)
+                if done:
+                    state = self.environment.reset()
+                done = torch.Tensor([done])
+                self.current_observation = torch.Tensor([state])
+                self.storage.add(step, value, reward, action_log_prob, entropy, done)
 
-    def compute_action_logs_and_entropies(self, probs, log_probs):
-        # TODO
-        return None
+            _, _, last_value = self.model(self.current_observation)
+            expected_reward = self.storage.compute_expected_reward(last_value,
+                                                                   self.params.discount_factor)
+            advantages = torch.tensor(expected_reward) - self.storage.values
+            value_loss = advantages.pow(2).mean()
+            policy_loss = -(advantages * self.storage.action_log_probs).mean()
+
+            self.optimizer.zero_grad()
+            loss = policy_loss - self.params.entropy_coef * self.storage.entropies.mean() + \
+                self.params.value_loss_coef * value_loss
+            loss.backward()
+            nn.utils.clip_grad_norm(self.model.parameters(), self.params.max_norm)
+            self._share_gradients()
+            self.optimizer.step()
+
+            if update % 20 == 0:
+                print('Process: {}. Update: {}. Loss: {}'.format(self.process_num,
+                                                                 update,
+                                                                 loss))
+
+    def compute_action_log_and_entropy(self, probs, log_probs):
+        values, indices = probs.max(1)
+        indices = indices.view(-1, 1)
+        action_log_prob = log_probs.gather(1, indices)
+
+        entropy = -(log_probs * probs).sum(-1)
+
+        return action_log_prob, entropy
+
+    def _share_gradients(self):
+        for local_param, global_param in zip(self.model.parameters(),
+                                             self.global_model.parameters()):
+            global_param._grad = local_param.grad
