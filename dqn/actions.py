3b69c4508cfce5b6eeeb28f215470f515b248ddb
Novica Sarenac <novicasarenac@gmail.com>
2019-05-21 12:03:16 +0200

    Use GPU to train DQN

--- dqn/actions.py
+++ dqn/actions.py
@@ -15,14 +15,14 @@ def get_action_space():
     return len(ACTIONS)
 
 
-def get_action(q_value, train=False, step=None, params=None):
+def get_action(q_value, train=False, step=None, params=None, device=None):
     if train:
         epsilon = params.epsilon_final + (params.epsilon_start - params.epsilon_final) * \
             math.exp(-1 * step / params.epsilon_step)
         if random.random() <= epsilon:
             action_index = random.randrange(get_action_space())
             action = ACTIONS[action_index]
-            return torch.LongTensor([action_index])[0], action
+            return torch.tensor([action_index], device=device)[0], action
     action_index = q_value.max(1)[1]
     action = ACTIONS[action_index[0]]
     return action_index[0], action
--- dqn/train.py
+++ dqn/train.py
@@ -12,28 +12,38 @@ class DQNTrainer:
     def __init__(self, params, model_path):
         self.params = params
         self.model_path = model_path
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.current_q_net = DQN(input_shape=1, num_of_actions=get_action_space())
+        self.current_q_net.to(self.device)
         self.target_q_net = DQN(input_shape=1, num_of_actions=get_action_space())
+        self.target_q_net.to(self.device)
         self.optimizer = RMSprop(self.current_q_net.parameters())
         self.replay_memory = ReplayMemory(self.params.memory_capacity)
         env = gym.make('CarRacing-v0')
         self.environment = EnvironmentWrapper(env, self.params.skip_steps)
 
     def run(self):
-        state = torch.Tensor(self.environment.reset())
+        state = torch.tensor(self.environment.reset(),
+                             device=self.device,
+                             dtype=torch.float32)
         self._update_target_q_net()
         for step in range(int(self.params.num_of_steps)):
             q_value = self.current_q_net(torch.stack([state]))
             action_index, action = get_action(q_value,
                                               train=True,
                                               step=step,
-                                              params=self.params)
+                                              params=self.params,
+                                              device=self.device)
             next_state, reward, done = self.environment.step(action)
-            next_state = torch.Tensor(next_state)
+            next_state = torch.tensor(next_state,
+                                      device=self.device,
+                                      dtype=torch.float32)
             self.replay_memory.add(state, action_index, reward, next_state, done)
             state = next_state
             if done:
-                state = torch.Tensor(self.environment.reset())
+                state = torch.tensor(self.environment.reset(),
+                                     device=self.device,
+                                     dtype=torch.float32)
             if len(self.replay_memory.memory) > self.params.batch_size:
                 loss = self._update_current_q_net()
                 print('Update: {}. Loss: {}'.format(step, loss))
@@ -48,8 +58,8 @@ class DQNTrainer:
         states = torch.stack(states)
         next_states = torch.stack(next_states)
         actions = torch.stack(actions).view(-1, 1)
-        rewards = torch.Tensor(rewards)
-        dones = torch.Tensor(dones)
+        rewards = torch.tensor(rewards, device=self.device)
+        dones = torch.tensor(dones, device=self.device, dtype=torch.float32)
 
         q_values = self.current_q_net(states).gather(1, actions)
         next_q_values = self.target_q_net(next_states).max(1)[0]
